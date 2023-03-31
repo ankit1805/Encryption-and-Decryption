06587b88bebb1b08298dc0e95ff3a34bbdc8ea66
Ankit Tiwari <99351747+ankit1805@users.noreply.github.com>
2023-03-31 13:15:27 +0530

    Update encrypt1.py

--- encrypt1.py
+++ encrypt1.py
@@ -1,40 +1,87 @@
-result = ''
-choice = ''
-message = ''
+#GUI FOR ENCRYPT AND DECRYPT
 
-while choice != 0:
-    choice = input("\n Do you want to encrypt or decrypt the message?\n 1 to encrypt, 2 to decrypt or 0 to exit program. ")
+from  tkinter import *
+from tkinter import messagebox
+import base64
 
-    if choice == '1':
-        message = input('\nEnter message for encryption: ')
-        for i in range(0, len(message)):
-            result = result + chr(ord(message[i]) -2)
+screen=Tk()
+screen.geometry("1080x1080")
+screen.title("Encode And Decode")
+screen.configure(bg="black")
 
-        print(result + '\n\n')
-        result = ''
+def encrypt():
+    password=code.get()
+    if password=="1234":
+        screen1=Toplevel(screen)
+        screen1.title("Encryption")
+        screen1.geometry("850x650")
+        screen1.configure(bg="pink")
+        
+        #Encryption process.
+        message=text1.get(1.0,END) #(1.0,END) means that fetch from  first line and 0 char to end char.
+        encode_message=message.encode("ascii")
+        base64_bytes=base64.b64encode(encode_message)
+        encrypt=base64_bytes.decode("ascii")
+        print(message)
+        
+        Label(screen1,text="Your text is encrypted ",font="impack 10 bold").place(x=5,y=6)
+        Label(print(message))
+        text2=Text(screen1,font="30",bd=4,wrap=WORD)
+        print(message)
+        text2.place(x=2,y=30,width=390,height=180)
+        text2.insert(END,encrypt)
+        print(message)
+    elif(password==""):
+        messagebox.showerror("ERROR","Please enter the secret key")
+    elif(password!="1234"):
+        messagebox.showerror("ERROR","Invalid secret key")
+        
+        
+def decrypt():
+    
+    password=code.get()
+    if password=="1234":
+        screen2=Toplevel(screen)
+        screen2.title("Decryption")
+        screen2.geometry("850x650")
+        screen2.configure(bg="black")
+        
+        #decryption process.
+        message=text1.get(1.0,END) #(1.0,END) means that fetch from  first line and 0 char to end char.
+        decode_message=message.encode("ascii")
+        base64_bytes=base64.b64decode(decode_message)
+        encrypt=base64_bytes.decode("ascii")
+        
+        Label(screen2,text="Your text is decrypted ",font="impack 10 bold").place(x=5,y=6)
+        text2=Text(screen2,font="30",bd=4,wrap=WORD)
+        text2.place(x=2,y=30,width=390,height=180)
+        text2.insert(END,encrypt)
+        
+    elif(password==""):
+        messagebox.showerror("ERROR","Please enter the secret key")
+    elif(password!="1234"):
+        messagebox.showerror("ERROR","Invalid secret key")
 
-    if choice == '2':
-        message = input('\nEnter message to decrypt: ')
-        for i in range(0, len(message)):
-            result = result + chr(ord(message[i]) + 2)
+def reset():
+    text1.delete(1.0,END)
+    code.set("")
+        
+#label
+Label(screen,text="Enter the text for encode and decode",font="impack 30 bold",bg="red").place(x=50,y=25)
+#text
+text1=Text(screen,font="20")
+text1.place(x=50,y=80,width=990,height=500)
 
-        print(result + '\n\n')
-        result = ''
+#label
+Label(screen,text="Enter secret key",font="impack 13 bold",bg="Yellow").place(x=520,y=600)
 
-    elif choice != '0':
-        print('Thanks to using this encryption and decryption. \n\n')
+#entery
+code=StringVar()
+Entry(textvariable=code,bd=8,font="20",show="*").place(x=500,y=650)
 
+#buttons
+Button(screen,text="Encrypt",font="arial 15 bold",bg="green",fg="white",command=encrypt).place(x=350,y=750)
+Button(screen,text="Decrypt",font="arial 15 bold",bg="green",fg="white",command=decrypt).place(x=550,y=750)
+Button(screen,text="Reset",font="arial 15 bold",bg="green",fg="white",command=reset).place(x=720,y=750)
 
-
-# from cryptography.fernet import Fernet
-# message =input("Enter your message")
-# key = Fernet.generate_key()
-# fernet = Fernet(key)
-# print("original string: ", message)
-
-# encMessage = fernet.encrypt(message.encode())
-# print("encrypted string: ", encMessage)
-
-
-# decMessage = fernet.decrypt(encMessage).decode()
-# print("decrypted string: ", decMessage)
+mainloop() 
